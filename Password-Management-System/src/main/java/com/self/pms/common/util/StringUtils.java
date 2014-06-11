1b70c040dd0684ed02a4d800dd30b65fb57ad4b6
ROHIT KHURANA <khurana.heaven.rohit@gmail.com>
2014-06-11 17:02:54 +0530

    Added one method in this class.

--- Password-Management-System/src/main/java/com/self/pms/common/util/StringUtils.java
+++ Password-Management-System/src/main/java/com/self/pms/common/util/StringUtils.java
@@ -4,9 +4,25 @@
 package com.self.pms.common.util;
 
 /**
+ * This is a class which contains all the string utilities methods.
+ * 
  * @author Rohit Khurana
  *
  */
 public class StringUtils {
 
+	/**
+	 * This method checks whether the specified string is empty or not.
+	 * 
+	 * @param str
+	 * @return true if empty otherwise false
+	 */
+	public boolean isEmpty(String str){
+		boolean isEmpty = false;
+		// Checking whether string is empty or not
+		if(str != null && str.length() > 0){
+			isEmpty = true;
+		}
+		return isEmpty;
+	}
 }
