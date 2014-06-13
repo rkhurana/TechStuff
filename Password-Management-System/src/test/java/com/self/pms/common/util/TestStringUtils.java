7dcae7ba734ddecf979a6059185ab58ba40b518b
ROHIT KHURANA <rkhurana@users.noreply.github.com>
2014-06-13 22:02:36 +0530

    Added test package and respective test class.

---
+++ Password-Management-System/src/test/java/com/self/pms/common/util/TestStringUtils.java
@@ -0,0 +1,19 @@
+package com.self.pms.common.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStringUtils {
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEqual() {
+		fail("Not yet implemented");
+	}
+
+}
